65995dc5d803bd7acad4118e7a9bdcf010ec8689
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-06-22 21:31:36 -0400

    added correct version of clicky

--- twoPhotonPlotting/clicky.m
+++ twoPhotonPlotting/clicky.m
@@ -1,47 +1,100 @@
-function [roi_points, intens] = clicky(movie_in, refimg)
+function [roi_points, greenCountMat, redCountMat] = clicky(greenMov,redMov, Stim, frameTimes, sumTitle,varargin)
+
+% Lets you select ROIS by left clicking to make a shape then right clicking
+% to finish that shape.
+% Then plots the fluorescence trace for that ROI
+% You can select mutliple ROIs.
+% A second right click prevents stops further ROIs from being drawn.
 
 
-if nargin < 2;
-    refimg = mean(movie_in, 3);
-end;
-
-nframes = size(movie_in, 3);
+%% Plot reference image
+refimg = mean(greenMov, 3);
 
 figure
-subplot(1,3,1)
+setCurrentFigurePosition(1) 
+subplot(2,2,1)
 imshow(refimg, [], 'InitialMagnification', 'fit')
 hold on;
+title('Mean image')
 
-[ysize, xsize] = size(refimg(:,:,1)); 
-npts = 1;
+%% Get image details
+nframes = size(greenMov, 3);
+[ysize, xsize] = size(refimg(:,:,1));
+
+
+%% Set colors
 colorindex = 0;
+ColorSet = distinguishable_colors(20,'b');
+set(gca, 'ColorOrder', ColorSet);
 order = get(gca,'ColorOrder');
+purple = [97 69 168]./255;
+
+%% Get ROIs
+npts = 1;
 nroi = 1;
-intens = [];
-  [x, y] = meshgrid(1:xsize, 1:ysize);
- while(npts > 0)
+greenCountMat = [];
+redCountMat = [];
+[x, y] = meshgrid(1:xsize, 1:ysize);
+while(npts > 0)
+    
+    % Draw the ROI
+    subplot(2,2,1)
+    [xv, yv] = (getline(gca, 'closed'));
+    if size(xv,1) < 3  % exit loop if only a line is drawn
+        break
+    end
+    % Generate the mask
+    inpoly = inpolygon(x,y,xv,yv);
+    
+    % Draw the bounding polygons and label them
+    currcolor = order(nroi,:);
+    plot(xv, yv, 'Linewidth', 1,'Color',currcolor);
+    text(mean(xv),mean(yv),num2str(nroi),'Color',currcolor,'FontSize',12);
+    
+    % Calculate the mean trace within the polygon
+    greenFCount = squeeze(sum(sum(greenMov.*repmat(inpoly, [1, 1, nframes]))))/sum(inpoly(:));
+    redFCount = squeeze(sum(sum(redMov.*repmat(inpoly, [1, 1, nframes]))))/sum(inpoly(:));
 
-  subplot(1,3,1)
-  [xv, yv] = (getline(gca, 'closed'));
-  if size(xv,1) < 3  % exit loop if only a line is drawn
-  break
-  end
-  inpoly = inpolygon(x,y,xv,yv);
-  
- %draw the bounding polygons and label them
-  currcolor = order(1+mod(colorindex,size(order,1)),:);
-  plot(xv, yv, 'Linewidth', 1,'Color',currcolor);
-  text(mean(xv),mean(yv),num2str(colorindex+1),'Color',currcolor,'FontSize',12);
-  
-  itrace = squeeze(sum(sum(movie_in.*repmat(inpoly, [1, 1, nframes]))))/sum(inpoly(:));
+    % Plot stimulus
+    subplot(2,2,2)
+    myplot(Stim.timeVec,Stim.stimulus,'Color',purple)
+    ylabel('Stimulus (V)')
+    set(gca,'xtick',[])
+    set(gca,'XColor','white')
+    title('Stimulus')
+    
+    % Plot the green trace
+    subplot(2,2,4)
+    hold on;
+    myplot(frameTimes,greenFCount,'Color',currcolor,'Linewidth',2);
+    colorindex = colorindex+1;
+    ylabel('F count')
+    xlabel('Time (s)')
+    title('Green Channel')
+    
+    % Plot the red trace
+    subplot(2,2,3)
+    hold on;
+    myplot(frameTimes,redFCount,'Color',currcolor,'Linewidth',2);
+    colorindex = colorindex+1;
+    ylabel('F count')
+    xlabel('Time (s)')
+    title('Red channel')
+    
+    % Figure formatting and add title 
+    spaceplots    
+    if exist('sumTitle','var')  
+        suptitle(sumTitle)
+    end    
+    set(gca,'FontName','Calibri')
+    
+    % Create a matrix of traces
+    greenCountMat = [greenCountMat; greenFCount'];
+    redCountMat = [redCountMat; redFCount'];
 
-  subplot(1,3,2:3) % plot the trace
-  hold on;
-  plot(itrace,'Color',currcolor);
-  colorindex = colorindex+1;
-  
-  intens = [intens; itrace'];
-  roi_points{nroi} = [xv, yv];
-  nroi = nroi + 1;
- end
- intens = intens';
+    % Store the rois
+    roi_points{nroi} = [xv, yv];
+    nroi = nroi + 1;
+end
+greenCountMat = greenCountMat';
+redCountMat = redCountMat';
