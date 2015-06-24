b12d44e2b76a8c41781d1404f8ad1ef35e0aad52
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-06-24 11:15:36 -0400

    more debugging and nicer plottting

--- twoPhotonPlotting/clickyMult.m
+++ twoPhotonPlotting/clickyMult.m
@@ -17,10 +17,9 @@ dateAsString = datestr(dateNumber,'mm-dd-yy');
 roiNum = getpref('scimSavePrefs','roiNum');
 blockNum = getpref('scimSavePrefs','blockNum');
 blockDescription = getpref('scimSavePrefs','blockDescrip');
-% sumTitle = [dateAsString,', ',exptInfo.prefixCode,', ','ExpNum ',num2str(exptInfo.expNum),', FlyNum ',num2str(exptInfo.flyNum),...
-%     ', RoiNum ',num2str(roiNum),', BlockNum ',num2str(blockNum)];
+roiDescription = getpref('scimSavePrefs','roiDescrip');
 sumTitle = {dateAsString;exptInfo.prefixCode;['ExpNum ',num2str(exptInfo.expNum)];['FlyNum ',num2str(exptInfo.flyNum)];...
-    ['RoiNum ',num2str(roiNum)];['BlockNum ',num2str(blockNum)]};
+    ['RoiNum ',num2str(roiNum)];['BlockNum ',num2str(blockNum)];blockDescription};
 
 %% Get mean movies
 meanGreenMov = mean(greenMov,4);
@@ -31,19 +30,16 @@ refimg = mean(meanGreenMov, 3);
 
 figure
 setCurrentFigurePosition(1)
-subplot(2,4,1)
-text(0.252,0.6,sumTitle,'FontSize',26)
-axis off
 
-y(1) = subplot(2,4,2);
+subplot(2,2,1);
 imshow(refimg, [], 'InitialMagnification', 'fit')
-hold on;
-title(blockDescription)
-axis image
+hold on 
+title(roiDescription)
+
 
 %% Plot stimulus
 purple = [97 69 168]./255;
-h(1) = subplot(2,4,3:4);
+h(1) = subplot(2,2,2);
 myplot(Stim.timeVec,Stim.stimulus,'Color',purple)
 ylabel('Stimulus (V)')
 set(gca,'xtick',[])
@@ -62,7 +58,6 @@ ColorSet = distinguishable_colors(20,'b');
 set(gca, 'ColorOrder', ColorSet);
 order = get(gca,'ColorOrder');
 
-
 %% Get ROIs
 npts = 1;
 nroi = 1;
@@ -73,7 +68,7 @@ numTrials = size(greenMov,4);
 while(npts > 0)
     
     % Draw the ROI
-    subplot(2,4,2)
+    subplot(2,2,1)
     [xv, yv] = (getline(gca, 'closed'));
     if size(xv,1) < 3  % exit loop if only a line is drawn
         break
@@ -99,9 +94,9 @@ while(npts > 0)
     redCountMat{nroi} = redFCount';
     
     % Plot the green trace
-    h(2) = subplot(2,4,7:8);
+    h(2) = subplot(2,2,4);
+    hold on 
     myplot(frameTimes,meanGreenFCount,'Color',currcolor,'Linewidth',2);
-    hold on
     myplot(frameTimes,greenFCount,'Color',currcolor,'Linewidth',1,'LineStyle','--');
     colorindex = colorindex+1;
     ylabel('F count')
@@ -109,29 +104,28 @@ while(npts > 0)
     title('Green Channel')
     
     % Plot the red trace
-    subplot(2,4,5:6)
+    h(3) = subplot(2,2,3);
+    hold on 
     myplot(frameTimes,meanRedFCount,'Color',currcolor,'Linewidth',2);
-    hold on;
     myplot(frameTimes,redFCount,'Color',currcolor,'Linewidth',1,'LineStyle','--');
     colorindex = colorindex+1;
     ylabel('F count')
     xlabel('Time (s)')
     title('Red channel')
     
-    % Figure formatting and add title
-    
-    spaceplots
-    tightfig;
-    set(gca,'FontName','Calibri')
-    set(0,'DefaultFigureColor','w')
-    linkaxes(h(:),'x')
-    linkaxes([h(1),y(1)],'y')
-    %     supTitle(sumTitle);
-    
     % Store the rois
     roi_points{nroi} = [xv, yv];
     nroi = nroi + 1;
 end
+
+% Figure formatting
+h = axes('position',[0,0,1,1],'visible','off');
+hold(h)
+text(0.01,0.8,sumTitle,'FontSize',26);
+spaceplots
+linkaxes(h(:),'x')
+set(gca,'FontName','Calibri')
+set(0,'DefaultFigureColor','w')
 
 %% Save Figure
 saveFolder = [flyPath,'\OnlineFigures\'];
