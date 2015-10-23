30fe51e661664906ec42cb2e5bcaa2e9673eb976
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-10-23 16:53:10 -0400

    append pdfs while only deleting summary file

--- plotUtils/myAppendPdfs.m
+++ plotUtils/myAppendPdfs.m
@@ -1,13 +1,14 @@
 function myAppendPdfs(filenames,groupFilename)
 
 % Delete old group pdf 
-fileNameCount = 1;
-while exist(groupFilename,'file')
-%     delete(groupFilename)
-    fileNameCount = fileNameCount + 1; 
-    groupFileStem = char(regexp(groupFilename,'.*(?=.pdf)','match'));
-    groupFilename = [groupFileStem,'v',num2str(fileNameCount),'.pdf'];
-end
+delete(groupFilename)
+% fileNameCount = 1;
+% while exist(groupFilename,'file')
+% %     delete(groupFilename)
+%     fileNameCount = fileNameCount + 1; 
+%     groupFileStem = char(regexp(groupFilename,'.*(?=.pdf)','match'));
+%     groupFilename = [groupFileStem,'v',num2str(fileNameCount),'.pdf'];
+% end
 
 % Append individual pdfs together
 append_pdfs(groupFilename,filenames{:})
