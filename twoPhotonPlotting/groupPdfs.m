581206452709db1c67f027ba4046033ec7d0127f
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-12-02 13:49:46 -0500

    groupPdfs to avoid multiple copies of same graph

--- twoPhotonPlotting/groupPdfs.m
+++ twoPhotonPlotting/groupPdfs.m
@@ -7,7 +7,11 @@ cd(folder)
 fileNames = dir('*.pdf'); 
 
 allFileStem = char(regexp(fileNames(1).name,'.*(?=roi)','match'));
-groupFileName = [folder,'\',allFileStem,'allFigs.pdf'];
+groupFileFolder = [folder,'\','SumFigs\'];
+if ~isdir(groupFileFolder) 
+    mkdir(groupFileFolder) 
+end
+groupFileName = [groupFileFolder,allFileStem,'allFigs.pdf'];
 
 saveFileNameArray = struct2cell(fileNames); 
 saveFileName = saveFileNameArray(1,:);
